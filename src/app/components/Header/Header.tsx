cf07eb6191aea2ad9b64b9e04da9efb9fcc5be95
siarhyenka <siarhyenka@gmail.com>
2024-01-05 16:04:06 +0300

    create project

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import './header.css'
+
+export const Header = () => {
+    return <>
+                <div className="header__content">
+                    <h2 className="header__title">Choose Your Option</h2>
+                    <p className="header__text">But I must explain to you how all this mistaken idea of denouncing </p>
+                </div>
+           </>
+}
---
+++ src/app/components/Main/Main.tsx
@@ -0,0 +1,22 @@
+import './main.css'
+
+export const Main = ()=> {
+    return (<>
+                <div className="container">
+                    <div className="main__content">
+                        <div className="content__left">
+                            <h3 className="left__title">FREELANCER</h3>
+                            <h2>Initially designed to</h2>
+                            <p>But I must explain to you how all this mistaken idea of denouncing </p>
+                            <button className="button">START HERE</button>
+                        </div>
+                        <div className="content__right">
+                        <h3 className="right__title">STUDIO</h3>
+                            <h2>Initially designed to</h2>
+                            <p>But I must explain to you how all this mistaken idea of denouncing </p>
+                            <button className="button">START HERE</button>
+                        </div>
+                    </div>
+                </div>
+            </>)
+}
